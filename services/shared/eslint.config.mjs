0bade99dba7fb309a910c3332541526f0c50b571
DeeCaaD <perttu.kangas@hotmail.fi>
2024-12-05 18:45:46 +0200

    Fix eslint

--- services/shared/eslint.config.mjs
+++ services/shared/eslint.config.mjs
@@ -1,19 +1,10 @@
 import eslint from '@eslint/js';
-import pluginSecurity from 'eslint-plugin-security';
 import tseslint from 'typescript-eslint';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.strict,
   ...tseslint.configs.stylistic,
-  {
-    plugins: {
-      security: pluginSecurity,
-    },
-    rules: {
-      ...pluginSecurity.configs.recommended.rules,
-    },
-  },
   {
     ignores: ['dist', 'node_modules', 'types/**/*.d.ts'],
   }
