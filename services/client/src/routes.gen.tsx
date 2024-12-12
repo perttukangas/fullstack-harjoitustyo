26d28f331d697adc3ed4f4521a85eae8556d87b7
DeeCaaD <perttu.kangas@hotmail.fi>
2024-12-12 15:57:32 +0200

    Update generated routes gen

--- services/client/src/routes.gen.tsx
+++ services/client/src/routes.gen.tsx
@@ -1,45 +1,30 @@
 // Generouted, changes to this file will be overridden
-import {
-  Outlet,
-  RouterProvider,
-  createLazyRoute,
-  createRootRoute,
-  createRoute,
-  createRouter,
-} from '@tanstack/react-router';
-import { Fragment } from 'react';
+import { Fragment } from 'react'
+import { Outlet, RouterProvider, createLazyRoute, createRootRoute, createRoute, createRouter } from '@tanstack/react-router'
 
-import NoMatch from './pages/404';
-import App from './pages/_app';
+import App from './pages/_app'
+import NoMatch from './pages/404'
 
-const root = createRootRoute({ component: App || Outlet });
-const _404 = createRoute({
-  getParentRoute: () => root,
-  path: '*',
-  component: NoMatch || Fragment,
-});
+const root = createRootRoute({ component: App || Outlet })
+const _404 = createRoute({ getParentRoute: () => root, path: '*', component: NoMatch || Fragment })
 const index = createRoute({ getParentRoute: () => root, path: '/' }).lazy(() =>
-  import('./pages/index').then((m) =>
-    createLazyRoute('/')({ component: m.default })
-  )
-);
-const profile = createRoute({
-  getParentRoute: () => root,
-  path: 'profile',
-}).lazy(() =>
-  import('./pages/profile').then((m) =>
-    createLazyRoute('/profile')({ component: m.default })
-  )
-);
+           import('./pages/index').then((m) => createLazyRoute('/')({ component: m.default }))
+         )
+const profile = createRoute({ getParentRoute: () => root, path: 'profile' }).lazy(() =>
+           import('./pages/profile').then((m) => createLazyRoute('/profile')({ component: m.default }))
+         )
 
-const config = root.addChildren([index, profile, _404]);
+const config = root.addChildren([
+  index,profile,
+  _404,
+])
 
-const router = createRouter({ routeTree: config });
-export const routes = config;
-export const Routes = () => <RouterProvider router={router} />;
+const router = createRouter({ routeTree: config })
+export const routes = config
+export const Routes = () => <RouterProvider router={router} />
 
 declare module '@tanstack/react-router' {
   interface Register {
-    router: typeof router;
+    router: typeof router
   }
 }
